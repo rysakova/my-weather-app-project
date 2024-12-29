7abaed39e01d781bbc3eff06c01bde065d306dee
rysakova <tanyarysakova@gmail.com>
2024-12-29 21:23:57 +0200

    added time parameters search from API

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ function updateWeather(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
 
     cityElement.innerHTML = response.data.city;
@@ -12,9 +14,21 @@ function updateWeather(response) {
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
 
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
     let apiKey = "440e344af069a0c2t1a109fc3o0b45ba";
