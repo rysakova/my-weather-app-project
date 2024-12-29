072df5b93002ff17bd4b2a998518cf7e6f11d1db
rysakova <tanyarysakova@gmail.com>
2024-12-29 20:51:40 +0200

    made JS search city work with updated temperature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 function updateWeather(response) {
-    
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    console.log (response.data.city);
 }
 
 
 
 function searchCity(city) {
     let apiKey = "440e344af069a0c2t1a109fc3o0b45ba";
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(updateWeather);
 }
 
@@ -15,8 +20,6 @@ function searchCity(city) {
 function searchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
 }
 
