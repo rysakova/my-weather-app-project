3c83f24d2ad4ba66c18eda1ec8977b115a2a03fa
rysakova <tanyarysakova@gmail.com>
2024-12-29 21:43:11 +0200

    added weather app icon from API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function updateWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
 
     cityElement.innerHTML = response.data.city;
@@ -15,6 +16,7 @@ function updateWeather(response) {
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
