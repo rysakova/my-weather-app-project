19ee23963e82fdac2527af5b459fdd9056ca3906
rysakova <tanyarysakova@gmail.com>
2024-12-29 21:05:19 +0200

    added weather app humidity parameter search from API

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ function updateWeather(response) {
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
 
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 }
 
 
