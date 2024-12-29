fe53406a1389942aa18dbe70e7f89d4ead6f0863
rysakova <tanyarysakova@gmail.com>
2024-12-29 21:09:11 +0200

    added weather app wind speed parameter search from API

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ function updateWeather(response) {
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
 
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 
