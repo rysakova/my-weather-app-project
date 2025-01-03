b631fbc07e109e3bfaa380ff84040b392b64076f
rysakova <tanyarysakova@gmail.com>
2025-01-03 18:16:25 +0200

    Inject the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -49,4 +49,28 @@ function searchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
-searchCity("Kyiv");
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml = 
+        forecastHtml +
+    `
+    <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">☁</div> 
+    <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature"><strong>15°C</strong></div> 
+        <div class="weather-forecast-temperature">9°C</div> 
+    </div>
+    </div>
+   `;
+   });
+
+   let forecastElement = document.querySelector("#forecast");
+   forecastElement.innerHTML = forecastHtml;
+ }
+
+ searchCity("Kyiv");
+ displayForecast();
