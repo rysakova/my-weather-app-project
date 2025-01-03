5cf557965b65171c7319fccc5ce8e7dfae5e3fdc
rysakova <tanyarysakova@gmail.com>
2025-01-03 21:12:25 +0200

    inject from API the day

--- src/index.js
+++ src/index.js
@@ -55,15 +55,23 @@ function getForecast(city) {
     axios(apiUrl).then(displayForecast);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"];
+
+    return days[date.getDay()];
+}
+
 function displayForecast(response) {
     let forecastHtml = "";
 
-    response.data.daily.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
         forecastHtml = 
         forecastHtml +
     `
     <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°C</strong></div> 
@@ -71,7 +79,7 @@ function displayForecast(response) {
     </div>
     </div>
    `;
-   });
+   }});
 
    let forecastElement = document.querySelector("#forecast");
    forecastElement.innerHTML = forecastHtml;
